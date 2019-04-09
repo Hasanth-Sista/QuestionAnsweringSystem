bc001283e8ee6817651973ce4719a112f96802c1
Hasanth <hasanth1993@gmail.com>
2019-04-09 16:52:21 -0500

    spacy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import nltk
+import corenlp
+
+from spacy.lang.en import English
+from spacy.pipeline import DependencyParser
+
+import spacy
 import os
+from nltk.corpus import wordnet as wn
 
 def main():
     # read directory and list all files
     directory = "WikipediaArticles"
     directoryFiles = os.listdir(directory)
+
     # read each file and split into sentences
     for file in directoryFiles:
         fileRead = open(directory + "/" + file, "r")
@@ -13,9 +21,36 @@ def main():
         # dummy = open("src/dummy1.txt", "w")
         for sentence in text:
             if sentence != '' and sentence != [] and len(sentence) != 0:
+                # words in each sentence
                 tokenized_text = sentence.split(" ")
                 # pos tags for words in sentences
                 pos_tags = nltk.pos_tag(tokenized_text)
+                #dependecy parser
+
+                nlp = English()
+                parser = nlp.create_pipe("parser")
+                parser = DependencyParser(nlp.vocab)
+                doc = nlp("This is a sentence.")
+                # This usually happens under the hood
+                processed = parser(doc)
+                nlp.begin_training()
+                print(processed)
+
+                #wordnet functions
+                synonyms = []
+                antonyms = []
+
+                for syn in wn.synsets("good"):
+                    for l in syn.lemmas():
+                        synonyms.append(l.name())
+                        if l.antonyms():
+                            antonyms.append(l.antonyms()[0].name())
+
+                # print(set(synonyms))
+                # print(set(antonyms))
+
+                break
+
        # dummy.write(str(pos_tags) + "\n\n")
 
 
